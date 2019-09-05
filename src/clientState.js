f9c2cffdb26a86fd1dc2f40ab1f828422a826e1d
Kim Jun Sik <jinaganda5@gmail.com>
2019-09-05 21:14:35 +0900

    Type Definitions fo Offline Schema

--- src/clientState.js
+++ src/clientState.js
@@ -1,3 +1,25 @@
-export const defaults = {};
-export const typeDefs = [];
+export const defaults = {
+  notes: []
+};
+export const typeDefs = [
+  `
+    schema {
+        query:Query
+        mutation:Mutation
+    }
+    type Query {
+        note(id:Int!):Note
+        notes: [Note]!
+    }
+    type Mutation {
+        createNote(title:String!, contents:String!):Note!
+        editNote(id:Int!, title:String!, contents:String!):Note!
+    }
+    type Note {
+        id:Int!
+        title:String!
+        contents:String!
+    }
+    `
+];
 export const resolvers = {};
